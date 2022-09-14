32f824059502528d6c843bdcbdb33b738ce69302
SwarnalataJena <jenaswarnalata600@gmail.com>
2022-09-14 15:24:06 +0530

    new updated

---
+++ src/test/java/Branch_tyss/BT1.java
@@ -0,0 +1,8 @@
+package Branch_tyss;
+
+public class BT1 {
+	public static void main(String[] args) {
+		System.out.println("this is testyantra");
+	}
+
+}
---
+++ src/test/java/git1/GIT2.java
@@ -0,0 +1,8 @@
+package git1;
+
+public class GIT2 {
+	public static void main(String[] args) {
+		System.out.println("this is akash");
+	}
+
+}
